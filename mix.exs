c586a8deb833640be736a8ac2a64e6f5e60dbcc6
Jan Lelis <mail@janlelis.de>
2014-07-20 12:14:23 +0200

    add more information to mix file

--- mix.exs
+++ mix.exs
@@ -5,6 +5,20 @@ defmodule YYID.Mixfile do
     [app: :yyid,
      version: "0.1.0",
      elixir: "~> 0.14.3",
-     deps: []]
+     deps: [],
+     package: package,
+     description: description]
+  end
+
+  defp description do
+    """
+Generates random tokens that look like type 4 UUIDs: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+"""
+  end
+
+  defp package do
+    [contributors: ["Jan Lelis"],
+     licenses: ["MIT"],
+     links: %{"github" => "https://github.com/janlelis/yyid.ex" }]
   end
 end
